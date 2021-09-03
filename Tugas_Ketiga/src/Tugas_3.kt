c0d810048e5b885606429d4501a0f671768d35fa
Rickyandikurniawan64 <39975146+rickyandi64@users.noreply.github.com>
2021-09-03 12:55:28 +0700

    Add files via upload

---
+++ Tugas_Ketiga/src/Tugas_3.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var angka: String
+    do {
+        print("Masukan Angka Berapa saja kecuali 5: ")
+        angka = readLine()!!
+
+    } while (angka != "5")
+    println("Kamu mengetik angka 5 Silahkan Coba Lagi ")
+}
